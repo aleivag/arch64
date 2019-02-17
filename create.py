b0d76883679eab2e8dcc0ee86f86b0e7a62c1e4f
Alvaro Leiva Geisse <aleivag@fb.com>
2019-02-16 23:42:32 -0400

    updated scripts

--- create.py
+++ create.py
@@ -5,17 +5,21 @@ from tempfile import TemporaryDirectory
 from subprocess import check_call
 from datetime import datetime
 
+def repeat(cmd, r=4):
+    return ' || '.join(cmd for _ in range(r))
+
 BOXES = Path('boxes')
 IMG_VER = sorted(BOXES .iterdir() if BOXES .is_dir() else [], reverse=True)
 if IMG_VER:
     IMG_VER = IMG_VER[0].absolute()
 
-#IMG_VER = 'aleivag/arch64'
+IMG_VER = 'aleivag/arch64'
 
 VAGRANT_FILE = f"""
 
 Vagrant.configure("2") do |config|
-  config.vm.box = "aleivag/arch64"
+  config.vm.box = "{IMG_VER}"
+  config.vm.box_check_update = true
 
   config.vm.synced_folder ".", "/vagrant", disabled: true
 
@@ -25,16 +29,18 @@ end
 
 """
 
-SETUP_SH = """#!/bin/bash
 
-# set -e
+SETUP_SH = f"""#!/bin/bash
 
-rm -rf /home/terry/ /home/vagrant/neofetch
+set -e
+set -x
+rm -rf /home/terry/
 
-pacman -Syy
-pacman --noconfirm -S cython ipython python-pip python-wheel vim git archlinux-keyring
-pacman --noconfirm -Su
-pacman --noconfirm -Sc
+{repeat("pacman --noconfirm -Syy")}
+{repeat("pacman --noconfirm -Sy archlinux-keyring")}
+{repeat("pacman --noconfirm -Sy cython ipython python-pip python-wheel vim git")}
+{repeat("pacman --noconfirm -Syu")}
+{repeat("pacman --noconfirm -Sc")}
 
 rm -f  /home/*/.bash_history /root/.bash_history
 rm -rf  /var/log/* /var/cache/pacman/*
@@ -43,7 +49,6 @@ rm -rf  /var/log/* /var/cache/pacman/*
 echo cleanup root
 dd if=/dev/zero of=/EMPTY bs=1M || /bin/true
 rm -f /EMPTY
-
 
 """
 
@@ -55,19 +60,25 @@ class Vagrant:
         self.vagrant_file = vagrant_file
         self.setup_sh = setup_sh
 
+    def _vagrant_call(self, cmd, *args, **kwargs):
+        check_call(['vagrant', cmd, *args], cwd=self.base, **kwargs)
+
+    def ssh(self):
+        self._vagrant_call('ssh')
+
     def up(self):
         (self.base / 'Vagrantfile').write_text(self.vagrant_file)
         (self.base / 'setup.sh').write_text(self.setup_sh)
-        check_call(['vagrant', 'up'], cwd=self.base)
+        self._vagrant_call('up')
+
 
     def destroy(self):
-        check_call(['vagrant', 'destroy', '--force'], cwd=self.base)
+        self._vagrant_call('destroy', '--force')
 
     def package(self, pkg):
-        check_call(['vagrant', 'package', '--output', pkg], cwd=self.base)
+        self._vagrant_call('package', '--output', pkg)
 
     def __enter__(self):
-        #self.up()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -81,6 +92,7 @@ with TemporaryDirectory() as tmpd, Vagrant(tmpd, VAGRANT_FILE, SETUP_SH) as vagr
     print("Creating Vagrantfile")
     print(VAGRANT_FILE)
     vagrant.up()
+    vagrant.ssh()
     vagrant.package(NEWPKG.absolute())
 
 
